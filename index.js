650bf9f5e940c6e37464f63938d6d7f8e07ce90b
usif <ynageeb03@gmail.com>
2024-05-28 18:09:45 +0300

    FIrst commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { prefix, token } = require('./home/config.json');
+
+const client = new Client({
+    intents: Object.values(GatewayIntentBits)
+});
+
+client.login(token);
+
+client.on('ready', () => {
+    console.log('Logged in as ' + client.user.tag);
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    const command = message.content.slice(prefix.length).split(' ')[0];
+
+    switch (command) {
+        case 'ping': {
+            message.reply({
+                content: `Pong! ${client.ws.ping}`,
+            });
+        };
+    };
+});
